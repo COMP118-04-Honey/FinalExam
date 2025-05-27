39b25b3edb7b23b26c888adfaf952bc8d0e29a18
EshiMeli <fedonasmel@gmail.com>
2025-05-27 15:39:01 +0300

    Doxygen for each function

--- FinalExam.cpp
+++ FinalExam.cpp
@@ -45,7 +45,12 @@ int main()
 	return 0;
 }
 
-
+//Functions
+/*
+* @brief Function to enter data into the 3x5 matrix.
+* @param n 3x5 matrix to store the data.
+* @details The function prompts the user to enter numbers within the range of -1.5 to 0.5.
+*/
 void EnterData(double n[3][5]) {
 	for (int i = 0; i < 3; i++) {
 		for (int j = 0; j < 5; j++) {
@@ -58,7 +63,12 @@ void EnterData(double n[3][5]) {
 		}
 	}
 }
-
+/*
+* @brief Function to calculate the sum of all negative numbers in the 3x5 matrix.
+* @param n 3x5 matrix to sum negative numbers.
+* @return The sum of all negative numbers in the matrix.
+* @details The function iterates through the matrix and adds up all negative numbers.
+*/
 double SumNeg(double n[3][5]) {
 	double sum = 0;
 	for (int i = 0; i < 3; i++) {
@@ -70,6 +80,12 @@ double SumNeg(double n[3][5]) {
 	}
 	return sum;
 }
+/*
+* @brief Function to count the number of elements with value 1.5 in the 3x5 matrix.
+* @param n 3x5 matrix to count elements.
+* @return The count of elements with value 1.5 in the matrix.
+* @details The function iterates through the matrix and counts how many times the value 1.5 appears.
+*/
 int CountElem(double n[3][5]) {
 	int count = 0;
 	for (int i = 0; i < 3; i++) {
